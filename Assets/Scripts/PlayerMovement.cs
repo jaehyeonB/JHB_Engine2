aa70342ac49a0773706adb807164867b01eae15e
JaehyeonB <didska5518@g.shingu.ac.kr>
2025-09-11 15:15:13 +0900

    [Feat] 집가서 과제 하기..
    VS 안돼는거 개킹받음

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    private float speed = 5f;
-
-    private CharacterController controller;
+    public float speed = 5f;
+    public jumpPower = 5f;
+    public gravity = -9.8f;
+    
+    private CharactorController controller;
+    private Vector3 velocity;
+    public bool isGrounded;
 
     void Start()
     {
@@ -16,10 +19,18 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        isGrounded = controller.isGrounded;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3 (h, 0, v);
+        Vector3 move = new Vector3 (x, 0, z);
         controller.Move(move * speed * Time.deltaTime);
+
+        if(isGrounded && Input.GetKeyDown(KeyDown.Space))
+        {
+            velocity.y = jumpPower;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
