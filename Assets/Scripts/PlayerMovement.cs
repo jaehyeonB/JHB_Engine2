52765cd4c147f9120a756ff83d7b1d8883086e28
JaehyeonB <didska5518@g.shingu.ac.kr>
2025-09-11 14:12:18 +0900

    [Feat] Repository 생성♡

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float speed = 5f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3 (h, 0, v);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
